2228ae29db5f6533769c823f8bc317b0b2169a68
jumaboyevagulsevar-crypto <jumaboyevagulsevar08@gmail.com>
2026-04-20 00:03:29 +0500

    Fix dark mode, cleanup sidebar and add library section

---
+++ library_data.js
@@ -0,0 +1,46 @@
+window.libraryData = {
+    books: [
+        {
+            title: "Küçük Prens",
+            author: "Antoine de Saint-Exupéry",
+            cover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1519777161i/38821034.jpg",
+            level: "A1-A2",
+            link: "https://www.readanybook.com/online/565457"
+        },
+        {
+            title: "İstanbul Hatırası",
+            author: "Ahmet Ümit",
+            cover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1360527394i/8505481.jpg",
+            level: "B2-C1",
+            link: "https://www.ahmetumit.com/kitaplar/istanbul-hatirasi"
+        },
+        {
+            title: "Simyacı",
+            author: "Paulo Coelho",
+            cover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1361138833i/107764.jpg",
+            level: "B1",
+            link: "https://www.readanybook.com/online/107764"
+        },
+        {
+            title: "Türkçe Dil Bilgisi",
+            author: "Tandem Yayınları",
+            cover: "https://cdn.dsmcdn.com/ty11/product/media/images/20200827/11/8786524/81467576/1/1_org_zoom.jpg",
+            level: "All Levels",
+            link: "https://www.pdfdrive.com/turkish-grammar-books.html"
+        },
+        {
+            title: "Kürk Mantolu Madonna",
+            author: "Sabahattin Ali",
+            cover: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1366242681i/11267800.jpg",
+            level: "B2",
+            link: "https://www.readanybook.com/online/11267800"
+        },
+        {
+            title: "Yunus Emre Seçme Şiirler",
+            author: "Yunus Emre",
+            cover: "https://images.thalia.media/00/-/2903828373f140689b9ad5b69992015a/yunus-emre-secmeler-tas-baski-ciltli-nobel-yayin-dagitim.jpeg",
+            level: "C1",
+            link: "https://www.antoloji.com/yunus-emre/"
+        }
+    ]
+};
